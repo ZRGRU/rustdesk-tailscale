5137d29f62ddcd27b47bee1534b7bd3c382a45e3
Lucas Evangelista <190989128+ZRGRU@users.noreply.github.com>
2025-07-08 16:47:27 -0300

    Adiciona scripts para backup de volumes do Docker: 'restaurar_volumes.sh' e 'salvar_volumes.sh'

---
+++ volumes/restaurar_volumes .sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script para backup de volumes do Docker
+
+CONTAINER_NAME="rustdesk-tailscale"
+VOLUME_NAME="rustdesk_data"
+
+sudo cp -r "./${CONTAINER_NAME}_${VOLUME_NAME}/_data" "/var/lib/docker/volumes/${CONTAINER_NAME}_${VOLUME_NAME}/_data"
+
