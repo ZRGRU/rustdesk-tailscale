1927ff90700efa31499e374d47aa5e5f11954b8c
Lucas Evangelista <190989128+ZRGRU@users.noreply.github.com>
2025-07-09 08:54:54 -0300

    Atualiza scripts de backup de volumes do Docker: restaura o script 'restaurar_volumes.sh' e adiciona mensagens de sucesso e erro ao script 'salvar_volumes.sh' e 'restaurar_volumes.sh'. Adiciona entradas ao .gitignore para ignorar diretórios de volumes.

--- volumes/salvar_volumes.sh
+++ volumes/salvar_volumes.sh
@@ -5,3 +5,9 @@ CONTAINER_NAME="rustdesk-tailscale"
 VOLUME_NAME="rustdesk_data"
 
 sudo cp -r "/var/lib/docker/volumes/${CONTAINER_NAME}_${VOLUME_NAME}/" ./
+
+if [ $? -eq 0 ]; then
+    echo "Backup do volume ${VOLUME_NAME} do contêiner ${CONTAINER_NAME} realizado com sucesso."
+else
+    echo "Erro ao realizar o backup do volume ${VOLUME_NAME} do contêiner ${CONTAINER_NAME}."
+fi
